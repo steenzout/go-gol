4dfc727629e0d9ea64b3948c3ef87fb738f4a226
steenzout <steenzout@ymail.com>
2015-11-04 14:58:31 -0700

    renamed severity field name

---
+++ fields/fields.go
@@ -0,0 +1,30 @@
+//
+// Copyright 2015 Rakuten Marketing LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package fields
+
+const (
+	// Message is the name of the log message attribute that stores a generic message.
+	Message = "message"
+	// Package is the name of the log message attribute that stores the name of the software package.
+	Package = "package"
+	// Severity is the name of the log message attribute that stores the severity level.
+	Severity = "severity"
+	// StartLevel is the name of the log message attribute that stores a start point in time.
+	Start = "start"
+	// StopLevel is the name of the log message attribute that stores a stop point in time.
+	Stop = "stop"
+)
