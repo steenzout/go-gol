6c0d615d0d5a3cdac7efae01371f943edce2cb39
steenzout <steenzout@ymail.com>
2015-11-05 12:44:17 -0700

    added severity filter.

---
+++ filters/severity.go
@@ -0,0 +1,24 @@
+package filters
+
+import (
+	"github.com/mediaFORGE/gol"
+	"github.com/mediaFORGE/gol/fields/severity"
+)
+
+type Severity struct {
+	minimum severity.Type
+}
+
+func NewSeverity(s severity.Type) gol.LogFilter {
+	return &Severity{
+		minimum: s,
+	}
+}
+
+func (f Severity) Filter(msg *gol.LogMessage) bool {
+	if s, err := msg.GetSeverity(); err != nil {
+		return false
+	} else {
+		return s >= f.minimum
+	}
+}
